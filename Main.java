c12e296f90f084d47b01eae02685860c9d77c217
adamlodeiro <adam.lodeiro@patelhs.org>
2021-01-29 15:27:16 +0000

    Have a base idea for project

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.scanner;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) 
+  {
+    System.out.println("Would you like to return your car?");
+    int carYear = scan.nextInt();
+    System.out.println("What year is your car from?");
+    int colorCar = scan.nextInt();
+    System.out.println("What year is it?");
+
+
+  }
+
+  static String typeOfCar(String car){
+    String theCar = "So you would like a " + car;
+    return theCar;
   }
 }
